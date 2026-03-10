fbf78980b7bd3678ccefb981fb6468b7e1d9d311
Akshata Madiwal <akshata@gmail.com>
2026-03-10 19:41:14 +0530

    Added root route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ import auth from "./routes/auth.js"
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Task Manager API is running 🚀");
+});
+
 app.use(cors());
 app.use(express.json());
 
@@ -22,7 +26,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
-app.get("/", (req, res) => {
-    res.send("Task Manager API is running 🚀");
 });
