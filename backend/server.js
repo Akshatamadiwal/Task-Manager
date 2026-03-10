40f3c040306c0f0b480af8fdac2da2a3c300fa94
Akshata Madiwal <akshata@gmail.com>
2026-03-10 19:45:51 +0530

    Use MongoDB Atlas for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,23 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import taskRoutes from "./routes/taskRoutes.js";
-import auth from "./routes/auth.js"
+import auth from "./routes/auth.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Task Manager API is running 🚀");
 });
 
-app.use(cors());
-app.use(express.json());
-
 // routes
 app.use("/", taskRoutes);
 app.use("/auth", auth);
 
-// DB connection
-mongoose.connect("mongodb://127.0.0.1:27017/todoDB")
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
